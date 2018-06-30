6c3fd9b966d47d452726925a80ec6dfd303414ed
blaisb <blais.bruno@gmail.com>
2018-06-30 09:29:46 -0400

    Some restructuration

--- functionalTests/sedimentation/sedimentationZaki-A-Form/launchParticle.sh
+++ functionalTests/sedimentation/sedimentationZaki-A-Form/launchParticle.sh
@@ -1,10 +1,19 @@
+#Initialize DEM
+cd DEM
+lmp_auto < in.liggghts_init
 rm DEM/post/*
+cd ..
+
+#Launch CFD-DEM
 cd CFD
 rm -r 0.*
+blockMesh
+decomposePar -force
 mpirun -np 2 cfdemSolverPiso -parallel > ../log
 #reconstructPar > ../reconstructLog
 cd ..
 
+#Post-Process
 python3 statsZaki.py output
 python3 plotSingleCaseZaki.py output
 
